74206af6fec2e53317dc90a5cc50cf62a7efe739
Russell Jahn <russelljahn@gmail.com>
2013-07-07 13:48:44 -0500

    Added skeleton file for parsing XML files.

---
+++ xmlParser.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+xmlParser.py
+
+Contains functions to parse crisis XML files into Django models.
+"""
+
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+
+
+def main():
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()

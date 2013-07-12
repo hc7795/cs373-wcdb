b3e1f6a8217347a51e272a2fa96e2d2f2f47ab2d
Young Seo <youngseo@cs.utexas.edu>
2013-07-11 19:17:38 -0500

    changed models, added admin file and templates

--- WorldCrisisDB/urls.py
+++ WorldCrisisDB/urls.py
@@ -1,17 +1,20 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'WorldCrisisDB.views.home', name='home'),
-    # url(r'^WorldCrisisDB/', include('WorldCrisisDB.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    (r'^crisis/$', 'wcdb.views.CrisesAll'),
 )
+
+"""
+	urlpatterns = patterns('',
+    url(r'^polls/$', 'polls.views.index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^admin/', include(admin.site.urls)),
+)"""
+
--- WorldCrisisDB/wcdb/views.py
+++ WorldCrisisDB/wcdb/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from wcdb.models import Crisis 
+
+def CrisesAll(request):
+  crises = Crisis.objects.all().order_by('crisisName')
+  context = { 'crises': crises }
+  #return render_to_response("crisesAll.html", context, context_instance-RequestContext(request))
+"""
+def PeopleAll(request):
+	people = Person.objects.all().order_by('name')
+	context = { 'people' : people }
+
+def OrganizationsAll(request):
+	organizations = Organization.objects.all().order_by('name')
+	context = { 'organizations' = organizations } 
+"""

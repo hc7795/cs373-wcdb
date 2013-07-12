0299ef10891528ef4373464cf099a3023147f652
Russell Jahn <russelljahn@gmail.com>
2013-07-12 18:14:43 -0500

    Hardcoded HTML pages are now functional.

--- urls.py
+++ urls.py
@@ -6,6 +6,18 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
+
+	# Hardcoded HTML pages.
+	url(r'CRI_IRAQWR', views.CRI_IRAQWR, name='CRI_IRAQWR'),
+	url(r'CRI_HURIKE', views.CRI_HURIKE, name='CRI_HURIKE'),
+	url(r'CRI_BAGAIR', views.CRI_BAGAIR, name='CRI_BAGAIR'),
+	url(r'ORG_WHLORG', views.ORG_WHLORG, name='ORG_WHLORG'),
+	url(r'ORG_REDCRS', views.ORG_REDCRS, name='ORG_REDCRS'),
+	url(r'ORG_IAVETA', views.ORG_IAVETA, name='ORG_IAVETA'),
+	url(r'PER_SADHUS', views.PER_SADHUS, name='PER_SADHUS'),
+	url(r'PER_BUSDAD', views.PER_BUSDAD, name='PER_BUSDAD'),
+	url(r'PER_BRAMAN', views.PER_BRAMAN, name='PER_BRAMAN'),
+
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^crisis/$', 'wcdb.views.CrisesAll'),
--- wcdb/views.py
+++ wcdb/views.py
@@ -18,9 +18,25 @@ def OrganizationsAll(request):
 """
 
 def index(request):
-	crises = Crisis.objects.all().order_by('CrisisName')
-	organizations = Organization.objects.all().order_by('OrganizationName')
-	people = Person.objects.all().order_by('PersonName')
+	# Uncomment below when we want to move on from hardcoded pages.
+	# crises = Crisis.objects.all().order_by('CrisisName')
+	# organizations = Organization.objects.all().order_by('OrganizationName')
+	# people = Person.objects.all().order_by('PersonName')
+
+	crises = []
+	crises.append(Crisis.objects.get(CrisisID="CRI_IRAQWR"))
+	crises.append(Crisis.objects.get(CrisisID="CRI_HURIKE"))
+	crises.append(Crisis.objects.get(CrisisID="CRI_BAGAIR"))
+
+	organizations = []
+	organizations.append(Organization.objects.get(OrganizationID="ORG_WHLORG"))
+	organizations.append(Organization.objects.get(OrganizationID="ORG_REDCRS"))
+	organizations.append(Organization.objects.get(OrganizationID="ORG_IAVETA"))
+
+	people = []
+	people.append(Person.objects.get(PersonID="PER_SADHUS"))
+	people.append(Person.objects.get(PersonID="PER_BUSDAD"))
+	people.append(Person.objects.get(PersonID="PER_BRAMAN"))
 
 	template = loader.get_template("splash.html")
 	context = RequestContext(request, {
@@ -30,3 +46,30 @@ def index(request):
 	})
 
 	return HttpResponse(template.render(context))
+
+def CRI_IRAQWR(request):
+	return HttpResponse("CRI_IRAQWR")
+
+def CRI_HURIKE(request):
+	return HttpResponse("CRI_HURIKE")
+
+def CRI_BAGAIR(request):
+	return HttpResponse("CRI_BAGAIR")
+
+def ORG_WHLORG(request):
+	return HttpResponse("ORG_WHLORG")
+
+def ORG_REDCRS(request):
+	return HttpResponse("ORG_REDCRS")
+
+def ORG_IAVETA(request):
+	return HttpResponse("ORG_IAVETA")
+
+def PER_SADHUS(request):
+	return HttpResponse("PER_SADHUS")
+
+def PER_BUSDAD(request):
+	return HttpResponse("PER_BUSDAD")
+
+def PER_BRAMAN(request):
+	return HttpResponse("PER_BRAMAN")

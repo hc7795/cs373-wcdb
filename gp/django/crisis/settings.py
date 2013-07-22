2e6e50113045c3a1e0a8788652545c6bc26b04de
hc7795 <hc7795@cs.utexas.edu>
2013-07-22 15:27:00 -0500

    Update settings.py

--- gp/django/crisis/settings.py
+++ gp/django/crisis/settings.py
@@ -12,9 +12,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cs373_hc7795',                      # Or path to database file if using sqlite3.
-        'USER': 'hc7795',                      # Not used with sqlite3.
-        'PASSWORD': 'e~wmbU1mGm',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'z',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }

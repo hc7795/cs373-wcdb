1d0d3acdd3b7f30b85f5d3dab331748ec2fc066e
Young Seo <youngseo@cs.utexas.edu>
2013-07-22 16:33:30 -0500

    messed with the css/js file for index. needs work

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 /* Main javascript script to be included on every page. */
-$(document).ready(
-	function() {
 
-		$('.column').masonry({
-			columnWidth: 200,
-			itemSelector: '.item'
-		});
-	}
-);
+$(document).ready( function() { 
+	var container = document.querySelector('#myContent');
+	var msnry = new Masonry( container, {
+	  columnWidth: 15,
+	  itemSelector: 'item'
+	});
+});
+

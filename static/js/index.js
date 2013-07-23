af9ad9864043703ec1cdbf38530e1b393c935de0
Young Seo <youngseo@cs.utexas.edu>
2013-07-22 23:59:42 -0500

    pics now in cols, but still messed up

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 /* Main javascript script to be included on every page. */
 
 $(document).ready( function() { 
-	var container = document.querySelector('#myContent');
+	var container = document.querySelector('#container');
 	var msnry = new Masonry( container, {
-	  columnWidth: 15,
-	  itemSelector: 'item'
+	  itemSelector: 'item',
+	  columnWidth: container.querySelector('.grid-sizer')
 	});
 });
 

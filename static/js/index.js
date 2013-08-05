c16141bb1565494a0f733eb6bf3b06cea7fbb3d6
russelljahn <russelljahn@gmail.com>
2013-08-05 00:17:10 -0500

    Cleaned up a lot of the css.

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 $(document).ready(
 	function() {
 		Galleria.loadTheme('../static/galleria_themes/classic/galleria.classic.min.js');
-    	Galleria.run('.galleria_container');
+    	Galleria.run('.index_galleria_container');
     	Galleria.configure({
     		showInfo : false,
     	});

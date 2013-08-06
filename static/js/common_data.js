11d8cb831650c2a1136b87b6c89b5847b08f6f61
russelljahn <russelljahn@gmail.com>
2013-08-05 19:37:26 -0500

    Added transitions and autoplay to Galleria galleries.

--- static/js/common_data.js
+++ static/js/common_data.js
@@ -2,10 +2,12 @@
 $(document).ready(
 	function() {
 		Galleria.loadTheme('../static/galleria_themes/classic/galleria.classic.min.js');
-    	Galleria.run('.common_galleria');
     	Galleria.configure({
     		showInfo : false,
     		dataSort : 'random',
+    		autoplay : true,
+    		transition : 'fade',
     	});
+    	Galleria.run('.common_galleria');
     }
 );
--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,12 @@
 
 $(document).ready(
 	function() {
-		Galleria.loadTheme('../static/galleria_themes/classic/galleria.classic.min.js');
-    	Galleria.run('.index_galleria_container');
+		Galleria.loadTheme('../static/galleria_themes/classic/galleria.classic.min.js');	
     	Galleria.configure({
     		showInfo : false,
+    		autoplay : true,
+    		transition : 'fade',
     	});
+    	Galleria.run('.index_galleria_container');
     }
 );
